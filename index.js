4f51ea35c60c26dcbbc960939c9d168385a42194
Anthony Flaherty <antrflaherty@gmail.com>
2021-05-14 15:33:18 +0100

    Show yesterady rain and today chance of rain

--- index.js
+++ index.js
@@ -52,10 +52,28 @@ function buildWeatherUrl(crag, uri) {
 function getTodayForecast(crag) {
 	let url = buildWeatherUrl(crag,FORECAST_URI);
 
-	const today = new Date();
-	const formattedTodayDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+	return new Promise((resolve, reject) => {
+		http.get(url, (res) => {
+			let data = '';
+			res.resume();
+			res.on('data', (chunk) => { data += chunk });
+			res.on('end', () => {
+				resolve(data);
+				if (!res.complete)
+					reject(
+						'The connection was terminated while the message was still being sent');
+			});
+		});
+	});
+}
 
-	url += `&dt=${formattedTodayDate}`;
+function getYesterdayWeather(crag) {
+	let url = buildWeatherUrl(crag,HISTORY_URI);
+
+	const now = Date.now();
+	const yesterday = new Date(now - 24 * 60 * 60 * 1000);
+	const yesterdayFormatted = `${yesterday.getFullYear()}-${yesterday.getMonth() + 1}-${yesterday.getDate()}`;
+	url += `&dt=${yesterdayFormatted}`;
 
 	return new Promise((resolve, reject) => {
 		http.get(url, (res) => {
@@ -73,21 +91,28 @@ function getTodayForecast(crag) {
 }
 
 async function addWeatherToCrag(crag){
-	const todayWeather = JSON.parse(await getTodayForecast(crag));
+	const forecast = JSON.parse(await getTodayForecast(crag));
+	const yesterday = JSON.parse(await getYesterdayWeather(crag));
 
-	return {...crag, todayWeather};
+	return {...crag, forecast, yesterday};
 }
 
-async function getWeatherValusAtCrags(crags) {
-	return  crags.map(addWeatherToCrag);
+function getWeatherValusAtCrags(crags) {
+	return crags.map(addWeatherToCrag);
 }
 
 async function run() {
 	let crags = await getCragsList();
 
-	crags = getWeatherValusAtCrags(crags.slice(0,10));
+	crags = await Promise.all(getWeatherValusAtCrags(crags.slice(0,20)));
 
-	console.log(crags);
-}
+	crags.forEach((crag) => {
+		console.log(
+`${crag.Title._text}
+Yesterday total precipitation: ${crag.yesterday.forecast.forecastday[0].day.totalprecip_mm}mm
+Chance of rain today: ${crag.forecast.forecast.forecastday[0].day.daily_chance_of_rain}%
+`);
+		});
+	}
 
 run();
