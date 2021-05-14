cd4c0898bc311baeb26f3292eae2fbee387d5b8a
Anthony Flaherty <antrflaherty@gmail.com>
2021-05-14 12:23:32 +0100

    Get crags from bmc

---
+++ index.js
@@ -0,0 +1,34 @@
+const https = require('https');
+const xml = require('xml-js');
+
+function getCragsList() {
+	return new Promise((resolve, reject) => {
+		https.get('https://www.thebmc.co.uk/Services/MapService.asmx/LoadCrags', (res) => {
+			let data = '';
+			res.resume();
+			res.on('data', (chunk) => { data += chunk });
+			res.on('end', () => {
+				resolve(extractNiceCragArray(data));
+				if (!res.complete)
+					reject(
+						'The connection was terminated while the message was still being sent');
+			});
+		});
+	});
+}
+
+function parseResponseToJson(data) {
+	return JSON.parse(xml.xml2json(data, { compact: true }));
+}
+
+function extractNiceCragArray(data) {
+	const parsedData = parseResponseToJson(data);
+	return cragArray = parsedData.ArrayOfCragMarker.CragMarker;
+}
+
+async function run() {
+	const crags = await getCragsList();
+	console.log(crags);
+}
+
+run();
