253b87cddd12c41f73ee3d55abe29277797ccf82
Anthony Flaherty <antrflaherty@gmail.com>
2021-05-14 12:37:21 +0100

    Get weather at first crag in list

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const https = require('https');
+const http = require('http');
 const xml = require('xml-js');
 const fs = require('fs');
 const path = require('path');
+
+const WEATHER_URL = 'http://api.weatherapi.com/v1/current.json'
+
 
 function getWeatherApiKey() {
 	const secretsFile = fs.readFileSync(path.resolve('secrets.json'));
@@ -33,9 +37,40 @@ function extractNiceCragArray(data) {
 	return cragArray = parsedData.ArrayOfCragMarker.CragMarker;
 }
 
+function buildWeatherUrl(crag) {
+	const lat = crag.Lat._text;
+	const long = crag.Lon._text;
+
+	const apiKey = getWeatherApiKey();
+
+	return `${WEATHER_URL}?key=${apiKey}&q=${lat},${long}`;
+
+}
+
+function getCurrentWeatherAtCrag(crag) {
+	const url = buildWeatherUrl(crag);
+
+
+	return new Promise((resolve, reject) => {
+		http.get(url, (res) => {
+			let data = '';
+			res.resume();
+			res.on('data', (chunk) => { data += chunk });
+			res.on('end', () => {
+				resolve(data);
+				if (!res.complete)
+					reject(
+						'The connection was terminated while the message was still being sent');
+			});
+		});
+	});
+}
+
 async function run() {
 	const crags = await getCragsList();
-	console.log(crags);
+
+	const weather = await getCurrentWeatherAtCrag(crags[0]);
+	console.log(weather);
 }
 
 run();
