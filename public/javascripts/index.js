a82391236278ef9eec2e9acd101d9d1be54e11c5
Anthony Flaherty <antrflaherty@gmail.com>
2021-06-11 13:56:07 +0100

    Use form instead

--- public/javascripts/index.js
+++
@@ -1,6 +0,0 @@
-function locationSubmitListener(event) {
-    const location = document.querySelector('input').value;
-    fetch(`/weather/local?location=${location}`).then(console.log);
-}
-
-document.querySelector('button').addEventListener('click', locationSubmitListener);
