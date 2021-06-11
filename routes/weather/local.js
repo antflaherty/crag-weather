4221c0105bb42a9962c37685b49c21fd12227532
Anthony Flaherty <antrflaherty@gmail.com>
2021-06-11 14:05:24 +0100

    Move crag weather code and reinstate dependencies

--- routes/weather/local.js
+++ routes/weather/local.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
     const {location} = req.query; 
-    console.log(`about to tell to render weather with ${location}`)
     res.render('weather', { location });
 });
 
