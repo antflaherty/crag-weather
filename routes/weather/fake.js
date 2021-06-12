682bf8808b815b294b3aeaaa0cf0d1a9a4bc2cf3
Anthony Flaherty <antrflaherty@gmail.com>
2021-06-12 18:14:17 +0100

    Add a static html

--- routes/weather/fake.js
+++ routes/weather/fake.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const path = require('path');
 
 router.get('/', function(req, res) {
+    res.redirect('/html/index.html');
+});
+router.get('/data', function(req, res) {
     const data = JSON.parse(fs.readFileSync(path.resolve('./bin/data/cragsWithExampleWeather.json')));
     res.send(data);
 });
